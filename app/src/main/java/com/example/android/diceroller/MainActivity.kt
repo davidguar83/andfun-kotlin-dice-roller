7b9765c5393a4803ac8766adcda04d60743e304c
david <davidrguardiola@hotmail.es>
2020-10-25 22:19:24 +0100

    cambios y comentarios echos

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -23,32 +23,32 @@ import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
+//contenedor de imagen
     lateinit var diceImage: ImageView
-
+// inicia el programa
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)//este metodo carga el layout que le indiquemos, activity_dos o el que esta ahora.
 
-        val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener {
-            rollDice()
+        val rollButton: Button = findViewById(R.id.mi_boton)//crea la variable rollbuton de tipo Button, findViewById= recoge del contenedor del layout con el id y nombre selecionado y nos lo mete en la variable antes declarada.
+        rollButton.setOnClickListener {//cada vez que le demos al botton se iniciara un Listener,  aciendo llamar al metodo rollDice(), que hace que se genere un numero aleatotio y nos muestre una imagen segun el numero selecionado
+            rollDice()//llama al metodo
         }
-
-        diceImage = findViewById(R.id.dice_image)
+//mete en el objeto el contenedor que le indiquemos con findViewById. findViewById= sirve para encontrar con el nombres de id del layout,lo que queremos cargar. gracias a la id que tienen en el contenedor de los layout..
+        diceImage = findViewById(R.id.imagen_dado) //mete en el objeto diceImage, lo que encuentre en el contenedor del layout con el id dice_image
     }
-
+// metodo que hace que los dados tengan valores diferentes.
     private fun rollDice() {
-        val randomInt = Random().nextInt(6) + 1
-        val drawableResource = when (randomInt) {
-            1 -> R.drawable.dice_1
+        val randomInt = Random().nextInt(6) + 1//mete en la variable randomInt un nº aleatorio entre 1-6
+        val drawableResource = when (randomInt) {//parecido al cause. almacena en la variable drawableResource el valor que tiene la variable randomInt y le asigna una imagen, de las que estan almacenadas en el directorio drawable, mediante el nombre de la imagen
+            1 -> R.drawable.lisa_loser//en vez de salir el dado 1, sale lisa llamandote loser
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
             4 -> R.drawable.dice_4
             5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
+            else -> R.drawable.actor_secundario//esta vez sale el actor secundario
         }
 
-        diceImage.setImageResource(drawableResource)
+        diceImage.setImageResource(drawableResource)//la imagen que tengo en (drawableResource)le asigno  con setImageResource  a la imagen del contenedor diceImagen
     }
 }
